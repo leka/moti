00bfba3578855ce0d7639098c44e1923c37e0762
Ladislas <ladislas@weareleka.com>
2013-07-03 12:09:57 +0200

    send binary to Pi functions
    Signed-off-by: Ladislas <ladislas@weareleka.com>

--- src/moti_bhvr_comRPi/moti_bhvr_comRPi.ino
+++ src/moti_bhvr_comRPi/moti_bhvr_comRPi.ino
@@ -26,38 +26,78 @@
 MOTI Moti = MOTI();
 
 
+
+int X_VALUE, Y_VALUE, Z_VALUE, YAW_VALUE, PITCH_VALUE, ROLL_VALUE;
+
+void sendBinaryInt(int value)
+{
+  // send the two bytes that comprise a two byte (16 bit) integer
+  // Serial.println("Data");
+  // Serial.write(lowByte(value));  // send the low byte
+  // Serial.write(highByte(value)); // send the high byte
+
+  Serial.println(lowByte(value));  // send the low byte
+  Serial.println(highByte(value));
+}
+
+void sendBinaryByte(byte value)
+{
+  Serial.write(value);
+}
+
 //#######//
 // SETUP //
 //#######//
 
 void setup() {
-	Moti.initVerbose();
+	Moti.init();
+
+	delay(500);
+
+	sendBinaryByte(INIT_PHASE);
 }
-
-
 
 //######//
 // LOOP //
 //######//
 
 void loop() {
-	Moti.goForward();
-	Moti.printRgbColor(35, 167, 210);
-	delay(5000);
-	Moti.printRgbColor(35, 167, 210);
-	Moti.blinkLed(4);
-	Moti.printRgbColor(35, 167, 210);
-	delay(2000);
-	Moti.printRgbColor(35, 167, 210);
-	Moti.spinRight();
-	Moti.printRgbColor(35, 167, 210);
-	delay(2000);
-	Moti.printRgbColor(35, 167, 210);
-	Moti.stop();
-	Moti.printRgbColor(35, 167, 210);
-	delay(2000);
-	Moti.printRgbColor(35, 167, 210);
-	Moti.blinkLed(10);
-	delay(1000);
 
+	Moti.checkSensors();
+
+	if(Serial.available() > 0){
+
+		int incomingByte = Serial.read();
+
+		sendBinaryByte(ANSWER_START);
+
+		sendBinaryByte(NBR_DATA);
+
+		sendBinaryByte(ACC_TYPE);
+
+		sendBinaryInt(0x06);
+		Serial.println("Bin Acc");
+		sendBinaryInt(Moti.getXYZ(0));
+		sendBinaryInt(Moti.getXYZ(1));
+		sendBinaryInt(Moti.getXYZ(2));
+
+		sendBinaryByte(GYR_TYPE);
+
+		sendBinaryInt(0x06);
+
+		int yaw = (int) Moti.getYPR(0);
+		int pitch =(int) Moti.getYPR(1);
+		int roll = (int) Moti.getYPR(2);
+Serial.println("Bin Gyr");
+		sendBinaryInt(yaw);
+		sendBinaryInt(pitch);
+		sendBinaryInt(roll);
+
+		// Serial.println((int) Moti.getYPR(0));
+		// Serial.println((int) Moti.getYPR(1));
+		// Serial.println((int) Moti.getYPR(2));
+		sendBinaryByte(ANSWER_END);
+	}
 }
+
+
