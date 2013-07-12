15a1f72261f95fe155597028bdd120df6693bc3a
Ladislas <ladislas@weareleka.com>
2013-07-12 12:58:45 +0200

    operational moti_main with serial command checking.
    Signed-off-by: Ladislas <ladislas@weareleka.com>

--- src/moti_main/moti_main.ino
+++ src/moti_main/moti_main.ino
@@ -24,7 +24,7 @@
 
 
 MOTI Moti = MOTI();
-String inData;
+
 
 //#######//
 // SETUP //
@@ -34,96 +34,25 @@ void setup() {
 	Moti.initVerbose();
 }
 
-void listenForSerialCommand();
-void serialServer();
-void serialRouter();
-
-
 //######//
 // LOOP //
 //######//
 
 void loop() {
 	/**
-	 * First check if serial commands have been sent to the robot.
+	 * First, we check if any serial commands were sent to the robot. If so, we enter the remote control state and stop the standard execution of void loop().
 	 */
-	listenForSerialCommand();
-
-
-	while(Serial.available() > 0){
-		char recieved = Serial.read();
-		inData += recieved;
-
-		if (recieved == '\n'){
-
-			if(inData == "help\n" || inData == "h\n"){
-				Serial.println(F("\n========== Moti's Help =========="));
-				Serial.println(F("h or help   ---> Show help"));
-				Serial.println(F("r or remote ---> Enable remote control of the robot"));
-				Serial.println(F("d or data   ---> Send sensors data"));
-				Serial.println(F("L/r/g/b     ---> Output color with led (e.g. L/125/26/213)"));
-				Serial.println(F("f           ---> Go forward"));
-				Serial.println(F("F/speed     ---> Go forward at chosen speed (e.g. F/200)"));
-				Serial.println(F("b           ---> Go backward"));
-				Serial.println(F("B/speed     ---> Go backward at chosen speed (e.g. F/200)"));
-				Serial.println(F("q or quit   ---> Quit remote control"));
-			}
-			else if(inData == "remote\n"){
-				Moti.setRemoteState(true);
-			}
-			else if(inData == "quit\n"){
-				Moti.setRemoteState(false);
-			}
-		}
-	}
+	Moti.serialServer();
 
 	/**
-	 * Then do the rest of the program
+	 * Then, if there is no serial command to execute, the robot goes on and lives a happy life.
 	 */
-	if(Moti.getRemoteState() == true){
-		Serial.println("Moti remote state is: true");
-	}
-	else{
-		Serial.println("Moti remote state is: false");
-	}
 
-	inData = ""; // Clear recieved buffer
+	Serial.println("Just chillin'...");
+
 	delay(Moti.getLoopDelay());
 }
 
-void listenForSerialCommand(){
-	while(Serial.available() > 0){
-		char recieved = Serial.read();
-		inData += recieved;
 
-		if (recieved == '\n'){
 
-			if(inData == "help\n" || inData == "h\n"){
-				Serial.println(F("\n========== Moti's Help =========="));
-				Serial.println(F("h or help   ---> Show help"));
-				Serial.println(F("r or remote ---> Enable remote control of the robot"));
-				Serial.println(F("d or data   ---> Send sensors data"));
-				Serial.println(F("L/r/g/b     ---> Output color with led (e.g. L/125/26/213)"));
-				Serial.println(F("f           ---> Go forward"));
-				Serial.println(F("F/speed     ---> Go forward at chosen speed (e.g. F/200)"));
-				Serial.println(F("b           ---> Go backward"));
-				Serial.println(F("B/speed     ---> Go backward at chosen speed (e.g. F/200)"));
-				Serial.println(F("q or quit   ---> Quit remote control"));
-			}
-			else if(inData == "r\n" || inData == "remote\n"){
-				Serial.println(F("Remote control activated"));
-				Moti.setRemoteState(true);
-			}
-			else {
-				Serial.println(F("Before sending command, you must enable the remote control mode by sending \"r\" or \"remote\"."));
-			}
-		}
-	}
-}
 
-void remoteControl(){
-	if(Moti.getRemoteSate()){
-		
-	}
-
-}
