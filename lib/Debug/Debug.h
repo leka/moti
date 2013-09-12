54997da2ce7703ead631eb43464e62b7de5cb45b
tinyladi <ladislas@weareleka.com>
2013-09-12 14:50:59 +0200

    change debug to work
    Signed-off-by: tinyladi <ladislas@weareleka.com>

--- lib/Debug/Debug.h
+++ lib/Debug/Debug.h
@@ -1,12 +1,14 @@
 #ifndef LEKA_MOTI_ARDUINO_DEBUG_H_
 #define LEKA_MOTI_ARDUINO_DEBUG_H_
 
+#ifndef debug
+#define debug false
+#endif
+
 #include <Arduino.h>
 #include <Serial.h>
 
 namespace Debug {
-
-	bool debug = false;
 
 	void log(String string){
 		if (debug) {
