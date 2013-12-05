ded9336ce3bc22733cb9b574cc7c24260af2722a
tinyladi <ladislas@weareleka.com>
2013-12-05 12:41:25 +0100

    test led class - fixe #26
    made some test with the class and methods and everything seems to be working fine

--- src/heart/heart.ino
+++ src/heart/heart.ino
@@ -5,13 +5,37 @@
 
 #include "Led.h"
 
-static WORKING_AREA(waThread1, 260);
+Led heart = Led(11, 12, 13);
 
-static msg_t Thread1(void *arg) {
+SEMAPHORE_DECL(output, 0);
+
+static WORKING_AREA(waLedThread, 260);
+static WORKING_AREA(waSerialThread, 260);
+
+static msg_t LedThread(void *arg) {
 
 	while (TRUE) {
+		serial.println("led start");
+		heart.shine(RAND);
+		chThdSleepMilliseconds(500);
+		chSemSignal(&output);
+		chThdSleepMilliseconds(500);
+		serial.println("led end");
+	}
+	return 0;
+}
 
+static msg_t SerialThread(void *arg) {
 
+	while (TRUE) {
+		chSemWait(&output);
+		serial.print("red : ");
+		serial.println(heart.getRgb(0));
+		serial.print("green : ");
+		serial.println(heart.getRgb(1));
+		serial.print("blue : ");
+		serial.println(heart.getRgb(2));
+		heart.turnOff();
 	}
 	return 0;
 }
@@ -22,11 +46,10 @@ void chSetup() {
 
 	delay(500);
 
-	chThdCreateStatic(waThread1, sizeof(waThread1),
-		NORMALPRIO, Thread1, NULL);
-
-
-
+	chThdCreateStatic(waLedThread, sizeof(waLedThread),
+		NORMALPRIO, LedThread, NULL);
+	chThdCreateStatic(waSerialThread, sizeof(waSerialThread),
+		NORMALPRIO, SerialThread, NULL);
 }
 
 
