fe04c8adcf6b79ecc06712b3a41d69bb258b887a
tinyladi <ladislas@weareleka.com>
2013-12-06 11:15:37 +0100

    working but not optimized #33

--- src/heart/heart.ino
+++ src/heart/heart.ino
@@ -15,28 +15,22 @@ static msg_t LedThread(void *arg) {
 	(void)arg;
 
 	volatile uint16_t basePwm = 0;
-	volatile uint16_t bpm = 60;
+	volatile uint8_t bpm = 60;
 	volatile uint16_t currentPwm = 0;
 	volatile uint16_t P = 70;
 	volatile uint16_t Q = 0;
-	volatile uint16_t R = 170;
+	volatile uint16_t R = 255;
 	volatile uint32_t waitDelay = 0;
 	volatile int32_t slice = 0;
 	volatile uint16_t i = 0;
 
 	while (TRUE) {
-		// for (i = basePwm; i < P; i++) {
-		// 	currentPwm = basePwm + 20 * sin(0.041 * i);
-		// 	heart.shine(currentPwm, 0, 0);
-		// 	chThdSleepMilliseconds(80/(P-basePwm));
-		// }
-
 		for (i = basePwm; i < P; i++) {
 			currentPwm = i;
 			heart.shine(currentPwm, 0, 0);
 			slice = P - basePwm;
 			waitDelay = 40000 / abs(slice);
-			serial.println(slice);
+			// serial.println(slice);
 			delayMicroseconds(waitDelay);
 		}
 
@@ -45,18 +39,18 @@ static msg_t LedThread(void *arg) {
 			heart.shine(currentPwm, 0, 0);
 			slice = P-basePwm;
 			waitDelay = 40000 / abs(slice);
-			serial.println(waitDelay);
+			// serial.println(waitDelay);
 			delayMicroseconds(waitDelay);
 		}
 
-		delayMicroseconds(60000);
+		chThdSleepMilliseconds(60);
 
 		for (i = basePwm; i > Q; i--) {
 			currentPwm = i;
 			heart.shine(currentPwm, 0, 0);
 			slice = P-basePwm;
 			waitDelay = 20000 / abs(slice);
-			serial.println(waitDelay);
+			// serial.println(waitDelay);
 			delayMicroseconds(waitDelay);
 		}
 
@@ -65,7 +59,7 @@ static msg_t LedThread(void *arg) {
 			heart.shine(currentPwm, 0, 0);
 			slice = P-basePwm;
 			waitDelay = 10000 / abs(slice);
-			serial.println(waitDelay);
+			// serial.println(waitDelay);
 			delayMicroseconds(waitDelay);
 		}
 
@@ -73,8 +67,8 @@ static msg_t LedThread(void *arg) {
 			currentPwm = i;
 			heart.shine(currentPwm, 0, 0);
 			slice = P-basePwm;
-			waitDelay = 12500 / abs(slice);
-			serial.println(waitDelay);
+			waitDelay = 25000 / abs(slice);
+			// serial.println(waitDelay);
 			delayMicroseconds(waitDelay);
 		}
 
@@ -83,16 +77,13 @@ static msg_t LedThread(void *arg) {
 			heart.shine(currentPwm, 0, 0);
 			slice = P-basePwm;
 			waitDelay = 20000 / abs(slice);
-			serial.println(waitDelay);
+			// serial.println(waitDelay);
 			delayMicroseconds(waitDelay);
 		}
 
-		delayMicroseconds(6000 / bpm);
-
 		heart.shine(basePwm, 0, 0);
-
-		delay(800);
-
+		serial.println(30000 / bpm);
+		delay(30000 / bpm);
 	}
 	return 0;
 }
