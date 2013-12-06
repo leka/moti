f8943972cea9bf0c5599af32627bddabb97992f1
tinyladi <ladislas@weareleka.com>
2013-12-06 08:05:26 +0100

    implement bpm

--- src/heart/heart.ino
+++ src/heart/heart.ino
@@ -2,72 +2,114 @@
 
 #include <Arduino.h>
 #include "ChibiOS_AVR.h"
+#include "Serial.h"
 
 #include "Led.h"
 
 Led heart = Led(11, 12, 13);
 
-SEMAPHORE_DECL(output, 0);
-
-static WORKING_AREA(waLedThread, 260);
-static WORKING_AREA(waSerialThread, 260);
+static WORKING_AREA(waLedThread, 1000);
 
 static msg_t LedThread(void *arg) {
 
-	while (TRUE) {
-		serial.println("led start");
-		heart.shine(RAND);
-		chThdSleepMilliseconds(500);
-		chSemSignal(&output);
-		chThdSleepMilliseconds(500);
-		serial.println("led end");
-	}
-	return 0;
-}
+	(void)arg;
 
-static msg_t SerialThread(void *arg) {
+	volatile uint16_t basePwm = 0;
+	volatile uint16_t bpm = 60;
+	volatile uint16_t currentPwm = 0;
+	volatile uint16_t P = 70;
+	volatile uint16_t Q = 0;
+	volatile uint16_t R = 170;
+	volatile uint32_t waitDelay = 0;
+	volatile int32_t slice = 0;
+	volatile uint16_t i = 0;
 
 	while (TRUE) {
-		chSemWait(&output);
-		serial.print("red : ");
-		serial.println(heart.getRgb(0));
-		serial.print("green : ");
-		serial.println(heart.getRgb(1));
-		serial.print("blue : ");
-		serial.println(heart.getRgb(2));
-		heart.turnOff();
+		// for (i = basePwm; i < P; i++) {
+		// 	currentPwm = basePwm + 20 * sin(0.041 * i);
+		// 	heart.shine(currentPwm, 0, 0);
+		// 	chThdSleepMilliseconds(80/(P-basePwm));
+		// }
+
+		for (i = basePwm; i < P; i++) {
+			currentPwm = i;
+			heart.shine(currentPwm, 0, 0);
+			slice = P - basePwm;
+			waitDelay = 40000 / abs(slice);
+			serial.println(slice);
+			delayMicroseconds(waitDelay);
+		}
+
+		for (i = P; i > basePwm; i--) {
+			currentPwm = i;
+			heart.shine(currentPwm, 0, 0);
+			slice = P-basePwm;
+			waitDelay = 40000 / abs(slice);
+			serial.println(waitDelay);
+			delayMicroseconds(waitDelay);
+		}
+
+		delayMicroseconds(60000);
+
+		for (i = basePwm; i > Q; i--) {
+			currentPwm = i;
+			heart.shine(currentPwm, 0, 0);
+			slice = P-basePwm;
+			waitDelay = 20000 / abs(slice);
+			serial.println(waitDelay);
+			delayMicroseconds(waitDelay);
+		}
+
+		for (i = Q; i < R; i++) {
+			currentPwm = i;
+			heart.shine(currentPwm, 0, 0);
+			slice = P-basePwm;
+			waitDelay = 10000 / abs(slice);
+			serial.println(waitDelay);
+			delayMicroseconds(waitDelay);
+		}
+
+		for (i = R; i > Q; i--) {
+			currentPwm = i;
+			heart.shine(currentPwm, 0, 0);
+			slice = P-basePwm;
+			waitDelay = 12500 / abs(slice);
+			serial.println(waitDelay);
+			delayMicroseconds(waitDelay);
+		}
+
+		for (i = Q; i < basePwm; i++) {
+			currentPwm = i;
+			heart.shine(currentPwm, 0, 0);
+			slice = P-basePwm;
+			waitDelay = 20000 / abs(slice);
+			serial.println(waitDelay);
+			delayMicroseconds(waitDelay);
+		}
+
+		delayMicroseconds(6000 / bpm);
+
+		heart.shine(basePwm, 0, 0);
+
+		delay(800);
+
 	}
 	return 0;
 }
 
 void chSetup() {
-
-	serial.println("Starting");
-
-	delay(500);
-
 	chThdCreateStatic(waLedThread, sizeof(waLedThread),
 		NORMALPRIO, LedThread, NULL);
-	chThdCreateStatic(waSerialThread, sizeof(waSerialThread),
-		NORMALPRIO, SerialThread, NULL);
 }
 
 
 void setup() {
-
 	serial.begin(115200);
-
-	delay(500);
-
-	serial.println("Starting");
-
 	chBegin(chSetup);
-
 	while(1) {
 	}
 }
 
 void loop() {
-
+	// nothing to do here
 }
-
