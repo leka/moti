bddf927d4799b993dfac4b303d4e24f742ff1825
tinyladi <ladislas@weareleka.com>
2013-12-07 09:28:14 +0100

    test by dividing the number of steps in for() by 10 and multiplying the intensity by 10 in heart.shine() - #33

--- src/heart/heart.ino
+++ src/heart/heart.ino
@@ -14,51 +14,51 @@ static msg_t HeartThread(void *arg) {
 
 	(void)arg;
 
-	volatile uint8_t basePwm = 10;
-	volatile uint8_t bpm = 30;
-	volatile uint8_t P = 80;
+	volatile uint8_t basePwm = 1; // divided by ten to have a wait delay higher than 1ms
+	volatile uint8_t bpm = 3;     // must multiply by ten in heart.shine();
+	volatile uint8_t P = 8;
 	volatile uint8_t Q = 0;
-	volatile uint8_t R = 255;
+	volatile uint8_t R = 25;
 	volatile uint32_t waitDelay = 0;
 	volatile uint8_t i = 0;
 
 	while (TRUE) {
 		for (i = basePwm; i < P; i++) {
-			heart.shine(i, 0, 0);
+			heart.shine(i * 10, 0, 0);
 			waitDelay = 40000 / (P - basePwm);
-			delayMicroseconds(waitDelay);
+			chThdSleepMicroseconds(waitDelay);
 		}
 
 		for (i = P; i > basePwm; i--) {
-			heart.shine(i, 0, 0);
+			heart.shine(i * 10, 0, 0);
 			waitDelay = 40000 / (P - basePwm);
-			delayMicroseconds(waitDelay);
+			chThdSleepMicroseconds(waitDelay);
 		}
 
 		chThdSleepMilliseconds(60);
 
 		for (i = basePwm; i > Q; i--) {
-			heart.shine(i, 0, 0);
+			heart.shine(i * 10, 0, 0);
 			waitDelay = 20000 / (basePwm - Q);
-			delayMicroseconds(waitDelay);
+			chThdSleepMicroseconds(waitDelay);
 		}
 
 		for (i = Q; i < R; i++) {
-			heart.shine(i, 0, 0);
+			heart.shine(i * 10, 0, 0);
 			waitDelay = 30000 / (R - Q);
-			delayMicroseconds(waitDelay);
+			chThdSleepMicroseconds(waitDelay);
 		}
 
 		for (i = R; i > Q; i--) {
-			heart.shine(i, 0, 0);
+			heart.shine(i * 10, 0, 0);
 			waitDelay = 80000 / (R - Q);
-			delayMicroseconds(waitDelay);
+			chThdSleepMicroseconds(waitDelay);
 		}
 
 		for (i = Q; i < basePwm; i++) {
-			heart.shine(i, 0, 0);
+			heart.shine(i * 10, 0, 0);
 			waitDelay = 20000 / (basePwm - Q);
-			delayMicroseconds(waitDelay);
+			chThdSleepMicroseconds(waitDelay);
 		}
 
 		// heart.shine(basePwm, 0, 0);
