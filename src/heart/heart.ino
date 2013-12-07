a2dfcfe7ae6fcab5463df3017f35b5d81fde5c47
tinyladi <ladislas@weareleka.com>
2013-12-07 12:56:27 +0100

    suppress for and do it by hand - #33
    because of the chThdSleep() issues, I deleted the for() to do everything by hand.
    not as smooth but seems to be working.
    gonna test it

--- src/heart/heart.ino
+++ src/heart/heart.ino
@@ -14,54 +14,45 @@ static msg_t HeartThread(void *arg) {
 
 	(void)arg;
 
-	volatile uint8_t basePwm = 1; // divided by ten to have a wait delay higher than 1ms
-	volatile uint8_t bpm = 3;     // must multiply by ten in heart.shine();
-	volatile uint8_t P = 8;
+	volatile uint8_t basePwm = 10; // divided by ten to have a wait delay higher than 1ms
+	volatile uint8_t bpm = 30;     // must multiply by ten in heart.shine();
+	volatile uint8_t P = 80;
 	volatile uint8_t Q = 0;
-	volatile uint8_t R = 25;
-	volatile uint32_t waitDelay = 0;
-	volatile uint8_t i = 0;
+	volatile uint8_t R = 255;
+	// volatile uint16_t waitDelay = 0;
+	// volatile uint8_t i = 0;
 
 	while (TRUE) {
-		for (i = basePwm; i < P; i++) {
-			heart.shine(i * 10, 0, 0);
-			waitDelay = 40000 / (P - basePwm);
-			chThdSleepMicroseconds(waitDelay);
-		}
 
-		for (i = P; i > basePwm; i--) {
-			heart.shine(i * 10, 0, 0);
-			waitDelay = 40000 / (P - basePwm);
-			chThdSleepMicroseconds(waitDelay);
-		}
+		heart.shine(basePwm, 0, 0);
+		chThdSleepMilliseconds(40/2);
+		heart.shine(P/2, 0, 0);
+		chThdSleepMilliseconds(40/2);
+		heart.shine(P, 0, 0);
+		chThdSleepMilliseconds(40/2);
+		heart.shine(P/2, 0, 0);
+		chThdSleepMilliseconds(40/2);
 
+		heart.shine(basePwm, 0, 0);
 		chThdSleepMilliseconds(60);
 
-		for (i = basePwm; i > Q; i--) {
-			heart.shine(i * 10, 0, 0);
-			waitDelay = 20000 / (basePwm - Q);
-			chThdSleepMicroseconds(waitDelay);
-		}
+		heart.shine(basePwm, 0, 0);
+		chThdSleepMilliseconds(30/2);
+		heart.shine(Q/2, 0, 0);
+		chThdSleepMilliseconds(30/2);
+		heart.shine(Q, 0, 0);
+		chThdSleepMilliseconds(30/2);
+		heart.shine(R/2, 0, 0);
+		chThdSleepMilliseconds(30/2);
+		heart.shine(R, 0, 0);
+		chThdSleepMilliseconds(30/2);
+		heart.shine(Q/2, 0, 0);
+		chThdSleepMilliseconds(30/2);
+		heart.shine(Q, 0, 0);
+		chThdSleepMilliseconds(20);
 
-		for (i = Q; i < R; i++) {
-			heart.shine(i * 10, 0, 0);
-			waitDelay = 30000 / (R - Q);
-			chThdSleepMicroseconds(waitDelay);
-		}
+		heart.shine(basePwm, 0, 0);
 
-		for (i = R; i > Q; i--) {
-			heart.shine(i * 10, 0, 0);
-			waitDelay = 80000 / (R - Q);
-			chThdSleepMicroseconds(waitDelay);
-		}
-
-		for (i = Q; i < basePwm; i++) {
-			heart.shine(i * 10, 0, 0);
-			waitDelay = 20000 / (basePwm - Q);
-			chThdSleepMicroseconds(waitDelay);
-		}
-
-		// heart.shine(basePwm, 0, 0);
 		chThdSleepMilliseconds(30000/ bpm);
 	}
 	return 0;
