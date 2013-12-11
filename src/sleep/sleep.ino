747517b755af8d331413d5a90d706ea7b8dcc457
tinyladi <ladislas@weareleka.com>
2013-12-11 12:50:31 +0100

    sleep thread - fix #35

--- src/sleep/sleep.ino
+++ src/sleep/sleep.ino
@@ -3,6 +3,7 @@
 #include <Arduino.h>
 #include "ChibiOS_AVR.h"
 #include "Serial.h"
+#include "Tools.h"
 
 #include "Led.h"
 
@@ -14,21 +15,21 @@ static msg_t SleepThread(void *arg) {
 
 	(void)arg;
 
-	volatile uint8_t basePwm = 10; // divided by ten to have a wait delay higher than 1ms
-	volatile uint8_t bpm = 40;     // must multiply by ten in heart.shine();
-	volatile uint8_t R = 200;
+	volatile uint8_t basePwm = 0; // divided by ten to have a wait delay higher than 1ms
+	volatile uint8_t bpm = 35;     // must multiply by ten in heart.shine();
+	volatile uint8_t R = 240;
 	// volatile uint16_t waitDelay = 0;
 	// volatile uint8_t i = 0;
 
 	while (TRUE) {
 
 		serial.println("Up");
-		sleep.fade(1500, 0, 0, 0, 0, basePwm, R);
+		sleep.fade(1300, 0, 0, 0, 0, basePwm, R);
 
 		// sleep.shine(0, 0, R);
 		// chThdSleepMilliseconds(200);
 		serial.println("Down");
-		sleep.fade(800, 0, 0, 0, 0, R, basePwm);
+		sleep.fade(650, 0, 0, 0, 0, R, basePwm);
 
 		// sleep.shine(0, 0, basePwm);
 		// heart.fade(40, RAND, RAND);
