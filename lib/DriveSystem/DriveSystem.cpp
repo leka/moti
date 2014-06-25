3ba505394edbc7566b707d6d873f54d4e5f62433
tinyladi <ladislas@weareleka.com>
2014-06-25 15:31:20 +0200

    add GPL3 licence to all libs

--- lib/DriveSystem/DriveSystem.cpp
+++ lib/DriveSystem/DriveSystem.cpp
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2013-2014 Ladislas de Toldi <ladislas at weareleka dot com> and Leka <http://weareleka.com>
+
+This file is part of Moti, a spherical robotic smart toy for autistic children.
+
+Moti is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Moti is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Moti. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Arduino.h>
 #include "DriveSystem.h"
 
