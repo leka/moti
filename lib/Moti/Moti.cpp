2235777c8a337017255b41eed29df6694865f879
ladislas <ladislas@weareleka.com>
2014-09-12 18:04:17 +0200

    namespace clean up

--- lib/Moti/Moti.cpp
+++ lib/Moti/Moti.cpp
@@ -1,152 +1,138 @@
 #include "Moti.h"
-#include "ChibiOS_AVR.h"
 
-#define HISTORY_SIZE 6
 
-void waitMs(uint16_t ms) {
-	chThdSleepMilliseconds(ms);
-}
 
 namespace Moti {
 
+	// VARIABLES
+
+	// Thread states
 	bool _isStarted = false;
 	bool _isRunning = false;
+
+	// Moti states
 	bool _isStuck = false;
-	bool _isShaken= false;
-	uint32_t _startStuckTime = 0;
+
+	bool _isShaken = false;
+	bool _isShakenX = false;
+	bool _isShakenY = false;
+	bool _isShakenZ = false;
+
 	bool _isSpinning = false;
-	float _spinAngle = 0.f;
+	bool _isSpinningY = false;
+	bool _isSpinningP = false;
+	bool _isSpinningR = false;
+
+	// Misc
 	int16_t _nLaps = 0;
+	float _spinAngle = 0.f;
+	float spinHistory[HISTORY_SIZE] = {0.f};
 
+	uint32_t _startStuckTime = 0;
 
-	float spinHistory[HISTORY_SIZE] = { 0.f };
-
-	msg_t thread(void* arg);
-
-	static WORKING_AREA(environmentThreadArea, 256);
-
-
-	/**
-	 * @brief Tells the Environment thread to run and check for events
-	 */
-	void run(void) {
-		_isRunning = true;
-		Serial.println(10);
-	}
-
-	/**
-	 * @brief Tells the Environment thread to stop checking for event
-	 */
-	void stop(void) {
-		_isRunning = false;
-		_isStuck = false;
-	}
-
-	/**
-	 * @brief Returns whether the device is stuck or not
-	 * @return true if it is stuck, false otherwise
-	 */
-	bool isStuck() {
-		if (!_isStarted)
-			start();
-
-		return _isStuck && (abs(millis() - _startStuckTime) > ENVIRONMENT_STUCK_TIME);
-	}
-
-	bool isShaken(void) {
-		return _isShaken;	
-	}
-
-	bool isSpinning(void) {
-		return _isSpinning;
-	}
-
-	uint8_t countSpinLaps(void) {
-		return (uint8_t)abs(_nLaps);
-	}
-
-
-	void start(void* arg, tprio_t priority) {
-		if (!_isStarted) {
-			_isStarted = true;
-
-			(void)chThdCreateStatic(environmentThreadArea,
-					sizeof(environmentThreadArea),
-					priority, thread, arg);
-
-		}
-	}
-
-	float mod(float a, float b) {
-		float m = (float)fmod((double)a, (double)b);
-
-		if (m < 0.f) {
-			m += b;
-		}
-
-		return m;
-	}
-
-	float _diffAngle(float a, float b) {
-		float diff = a - b;
-
-		return mod(diff + 180.f, 360.f) - 180.f;
-	}
-
-	float _arrayDeltaSum(float* array, uint8_t size) {
-		float sum = abs(_diffAngle(array[0], array[size - 1]));
-
-		for (uint8_t i = 1; i < size; ++i)
-			sum += abs(_diffAngle(array[i], array[i - 1]));
-
-		return sum;
-	}
-
-	msg_t thread(void* arg) {
-		uint8_t i = 0;
-
-		float currentAngle = 0.f;
-		float oldAngle = 0.f;
-
-		while (!chThdShouldTerminate()) {
-			if (_isRunning) {
-
-				if (abs(Sensors::getAccX()) > ENVIRONMENT_STUCK_THRESHOLD) {
-					if (!_isStuck) {
-						_isStuck = true;
-						_startStuckTime = millis();
-					}
-				}
-				else
-					_isStuck = false;
-
-
-				oldAngle = currentAngle;
-				currentAngle = Sensors::getEulerPhiDeg();
-				spinHistory[i] = currentAngle;
-				i = (i + 1) % HISTORY_SIZE;
-
-				if (abs(_arrayDeltaSum(spinHistory, HISTORY_SIZE)) > 15.f)
-					_isSpinning = true;
-				else {
-					_isSpinning = false;
-					_nLaps = 0;
-				}
-
-				if (_isSpinning) {
-					if ((currentAngle * oldAngle < 0.f) && (currentAngle * oldAngle > -8000.f)) {
-						if (currentAngle > 0.f)
-							_nLaps++;
-						else
-							_nLaps--;
-					}
-				}
-			}
-
-			waitMs(ENVIRONMENT_THREAD_DELAY);
-		}
-
-		return (msg_t)0;
-	}
+	static WORKING_AREA(motiModuleThreadArea, 256);
 
 }
+
+
+/**
+ * @brief Tells the Environment thread to run and check for events
+ */
+void Moti::run(void) {
+	_isRunning = true;
+	Serial.println(10);
+}
+
+/**
+ * @brief Tells the Environment thread to stop checking for event
+ */
+void Moti::stop(void) {
+	_isRunning = false;
+	_isStuck = false;
+}
+
+/**
+ * @brief Returns whether the device is stuck or not
+ * @return true if it is stuck, false otherwise
+ */
+bool Moti::isStuck() {
+	if (!_isStarted)
+		start();
+
+	return _isStuck && (abs(millis() - _startStuckTime) > ENVIRONMENT_STUCK_TIME);
+}
+
+bool Moti::isShaken(void) {
+	return _isShaken;
+}
+
+bool Moti::isSpinning(void) {
+	return _isSpinning;
+}
+
+uint8_t Moti::getLapsZ(void) {
+	return (uint8_t)abs(_nLaps);
+}
+
+
+void Moti::start(void* arg, tprio_t priority) {
+	if (!_isStarted) {
+		_isStarted = true;
+
+		(void)chThdCreateStatic(motiModuleThreadArea,
+				sizeof(motiModuleThreadArea),
+				priority, moduleThread, arg);
+
+	}
+}
+
+msg_t Moti::moduleThread(void* arg) {
+
+	(void) arg;
+
+	uint8_t i = 0;
+
+	float currentAngle = 0.f;
+	float oldAngle = 0.f;
+
+	while (!chThdShouldTerminate()) {
+		if (_isRunning) {
+
+			if (abs(Sensors::getAccX()) > ENVIRONMENT_STUCK_THRESHOLD) {
+				if (!_isStuck) {
+					_isStuck = true;
+					_startStuckTime = millis();
+				}
+			}
+			else
+				_isStuck = false;
+
+
+			oldAngle = currentAngle;
+			currentAngle = Sensors::getEulerPhiDeg();
+			spinHistory[i] = currentAngle;
+			i = (i + 1) % HISTORY_SIZE;
+
+			if (abs(Toolbox::arrayDeltaSum(spinHistory, HISTORY_SIZE)) > 15.f)
+				_isSpinning = true;
+			else {
+				_isSpinning = false;
+				_nLaps = 0;
+			}
+
+			if (_isSpinning) {
+				if ((currentAngle * oldAngle < 0.f) && (currentAngle * oldAngle > -8000.f)) {
+					if (currentAngle > 0.f)
+						_nLaps++;
+					else
+						_nLaps--;
+				}
+			}
+		}
+
+		waitMs(ENVIRONMENT_THREAD_DELAY);
+	}
+
+	return (msg_t)0;
+}
+
--- lib/Moti/Moti.h
+++ lib/Moti/Moti.h
@@ -1,24 +1,25 @@
 /*
-	 Copyright (C) 2013-2014 Ladislas de Toldi <ladislas at weareleka dot com> and Leka <http://weareleka.com>
+   Copyright (C) 2013-2014 Ladislas de Toldi <ladislas at weareleka dot com>
+   and Leka <http://weareleka.com>
 
-	 This file is part of Moti, a spherical robotic smart toy for autistic children.
+   This file is part of Moti, a spherical robotic smart toy for autistic children.
 
-	 Moti is free software: you can redistribute it and/or modify
-	 it under the terms of the GNU General Public License as published by
-	 the Free Software Foundation, either version 3 of the License, or
-	 (at your option) any later version.
+   Moti is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-	 Moti is distributed in the hope that it will be useful,
-	 but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 GNU General Public License for more details.
+   Moti is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-	 You should have received a copy of the GNU General Public License
-	 along with Moti. If not, see <http://www.gnu.org/licenses/>.
-	 */
+   You should have received a copy of the GNU General Public License
+   along with Moti. If not, see <http://www.gnu.org/licenses/>.
+   */
 
-#ifndef LEKA_MOTI_ARDUINO_MOTI_H_
-#define LEKA_MOTI_ARDUINO_MOTI_H_
+#ifndef LEKA_MOTI_MODULE_MOTI_H_
+#define LEKA_MOTI_MODULE_MOTI_H_
 
 /**
  * @file Moti.h
@@ -26,31 +27,40 @@
  * @version 1.0
  */
 
-#include <math.h>
-
+#include <Arduino.h>
 #include "ChibiOS_AVR.h"
-// #include "Color.h"
+#include "Toolbox.h"
 #include "Sensors.h"
 
+#define HISTORY_SIZE 6
 
 void waitMs(uint16_t ms);
 
 namespace Moti {
-	void start(void* arg=NULL, tprio_t priority=NORMALPRIO+1);
 
+	// Thread
+	msg_t moduleThread(void* arg);
+	void start(void* arg=NULL, tprio_t priority=NORMALPRIO+1);
 	void run(void);
 	void stop(void);
 
+	// Methods
+	uint8_t getLapsZ(void);
+
+	// States
 	bool isStuck(void);
+	bool isFalling(void);
+
 	bool isShaken(void);
 	bool isShakenX(void);
 	bool isShakenY(void);
 	bool isShakenZ(void);
-	bool isFalling(void);
 
 	bool isSpinning(void);
-	uint8_t countSpinLaps(void);
-}
+	bool isSpinningY(void);
+	bool isSpinningP(void);
+	bool isSpinningR(void);
 
+}
 
 #endif
