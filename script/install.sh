acd0081365415ec8eb077ec9ba8004d10f8a5478
Ladislas <ladislas@weareleka.com>
2014-11-09 22:35:26 +0100

    update travis script with new arduino versions

--- script/install.sh
+++ script/install.sh
@@ -2,21 +2,21 @@
 
 echo "Installing dependencies needed to build the sources and tests..."
 
-ARDUINO_BASENAME="arduino-1.0.5"
+ARDUINO_BASENAME="arduino-1.0.6"
 ARDUINO_FILE="$ARDUINO_BASENAME-linux64.tgz"
-ARDUINO_URL="http://arduino.googlecode.com/files/$ARDUINO_FILE"
+ARDUINO_URL="http://arduino.cc/download.php?f=/$ARDUINO_FILE"
 
-AVR_GCC_BASENAME="arduino-1.5.7"
+AVR_GCC_BASENAME="arduino-1.5.8"
 AVR_GCC_FILE="$AVR_GCC_BASENAME-linux64.tgz"
-AVR_GCC_URL="http://downloads.arduino.cc/$AVR_GCC_FILE"
+AVR_GCC_URL="http://arduino.cc/download.php?f=/$AVR_GCC_FILE"
 
-echo "Installing Arduino 1.0.5..."
+echo "Installing Arduino 1.0.6..."
 
 wget -q "$ARDUINO_URL"
 tar -xzf "$ARDUINO_FILE"
 sudo mv "$ARDUINO_BASENAME/" "$ARDUINO"
 
-echo "Installing avr-gcc from Arduino 1.5.7..."
+echo "Installing avr-gcc from Arduino 1.5.8..."
 
 wget -q "$AVR_GCC_URL"
 tar -xzf "$AVR_GCC_FILE"
