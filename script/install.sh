dd0827a398112eadce71fcd3e9a6096a70d1d1d9
Ladislas <ladislas@weareleka.com>
2014-11-09 23:22:40 +0100

    update CI scripts

--- script/install.sh
+++ script/install.sh
@@ -12,13 +12,13 @@ AVR_GCC_URL="http://arduino.cc/download.php?f=/$AVR_GCC_FILE"
 
 echo "Installing Arduino 1.0.6..."
 
-wget "$ARDUINO_URL"
+wget "$ARDUINO_URL" -O "$ARDUINO_FILE"
 tar -xzf "$ARDUINO_FILE"
 sudo mv "$ARDUINO_BASENAME/" "$ARDUINO"
 
 echo "Installing avr-gcc from Arduino 1.5.8..."
 
-wget "$AVR_GCC_URL"
+wget "$AVR_GCC_URL" -O "AVR_GCC_FILE"
 tar -xzf "$AVR_GCC_FILE"
 sudo mv "$AVR_GCC_BASENAME/hardware/tools/avr" "$AVR_GCC"
 
