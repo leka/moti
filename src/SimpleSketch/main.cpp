8870143d20116508ae3c924de94a7c6725da894e
Ladislas <ladislas@weareleka.com>
2014-11-10 12:01:48 +0100

    add SimpleSketch for testing purposes

---
+++ src/SimpleSketch/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+// #include <Wire.h>
+//
+// #include "Light.h"
+
+void setup() {
+
+	Serial.begin(9600);
+
+}
+
+void loop() {
+
+	Serial.println("Hello, World!");
+	delay(1000);
+
+}
