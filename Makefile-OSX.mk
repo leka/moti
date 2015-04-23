a6d85c4614168347c5d7213af4e464c7011578de
Ladislas de Toldi <ladislas@leka.io>
2015-04-23 14:14:26 +0200

    update makefile to work with Arduino IDE 1.6.x

--- Makefile-OSX.mk
+++ Makefile-OSX.mk
@@ -7,13 +7,17 @@
 ### This is the path to where you have created/cloned your project
 PROJECT_DIR       = /Users/ladislas/dev/leka/moti
 
+### AVR_GCC_VERSION
+### Check if the version is equal or higher than 4.9
+AVR_GCC_VERSION  := $(shell expr `avr-gcc -dumpversion | cut -f1` \>= 4.9)
+
 ### ARDMK_DIR
 ### Path to the Arduino-Makefile directory.
 ARDMK_DIR         = $(PROJECT_DIR)/Arduino-Makefile
 
 ### ARDUINO_DIR
 ### Path to the Arduino application and ressources directory.
-ARDUINO_DIR       = /Applications/Arduino.app/Contents/Resources/Java
+ARDUINO_DIR       = /Applications/Arduino.app/Contents/Java
 
 ### USER_LIB_PATH
 ### Path to where the your project's libraries are stored.
@@ -21,7 +25,8 @@ USER_LIB_PATH     :=  $(realpath $(PROJECT_DIR)/lib)
 
 ### BOARD_TAG
 ### It must be set to the board you are currently using. (i.e uno, mega2560, etc.)
-BOARD_TAG         = mega2560
+BOARD_TAG         = mega
+BOARD_SUB         = atmega2560
 
 ### MONITOR_BAUDRATE
 ### It must be set to Serial baudrate value you are using.
@@ -35,10 +40,6 @@ AVR_TOOLS_DIR     = /usr/local
 ### Path to avrdude directory.
 AVRDDUDE          = /usr/local/bin/avrdude
 
-### AUTO_LIB
-### Auto link libraries
-AUTO_LIB = 1
-
 ### CFLAGS_STD
 CFLAGS_STD        = -std=gnu11
 
@@ -48,10 +49,6 @@ CXXFLAGS_STD      = -std=gnu++14
 ### CPPFLAGS
 ### Flags you might want to set for debugging purpose. Comment to stop.
 CXXFLAGS         = -pedantic -Wall -Wextra
-
-### AVR_GCC_VERSION
-### Check if the version is equal or higher than 4.9
-AVR_GCC_VERSION  := $(shell expr `avr-gcc -dumpversion | cut -f1` \>= 4.9)
 
 ### If avr-gcc -v is higher than 4.9, activate coloring of the output
 ifeq "$(AVR_GCC_VERSION)" "1"
@@ -63,10 +60,8 @@ endif
 MONITOR_PORT      = /dev/tty.usbmodem*
 
 ### don't touch this
-CURRENT_DIR       = $(shell basename $(CURDIR))
-
-### OBJDIR
 ### This is were you put the binaries you just compile using 'make'
+CURRENT_DIR       = $(shell basename $(CURDIR))
 OBJDIR            = $(PROJECT_DIR)/bin/$(CURRENT_DIR)/$(BOARD_TAG)
 
 ### path to Arduino.mk, inside the ARDMK_DIR, don't touch.
